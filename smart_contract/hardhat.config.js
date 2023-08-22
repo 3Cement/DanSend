7c50de9508ff81ffe78b0849a9008b90e6ac4645
daniel <danielmilewski123@gmail.com>
2023-08-22 12:32:54 +0200

    deploy file created

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
-};
+  solidity: '0.8.0',
+  networks: {
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/qUGgRY0dOFSDjKaJ6V09wVADx4VE2FAI',
+      accounts: ['']
+    }
+  }
+}
